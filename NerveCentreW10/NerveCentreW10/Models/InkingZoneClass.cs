801a5a62392e1b3215830ff848cf82e1756d56de
Ben Malloy <benmalloy@outlook.com>
2018-12-30 17:43:48 +0000

    Section6Inking Zone added.

---
+++ NerveCentreW10/NerveCentreW10/Models/InkingZoneClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NerveCentreW10.Models
+{
+    public class InkingZoneClass
+    {
+        public string InkingZoneTitle { get; set; }
+        public Uri InkingZoneImage { get; set; }
+        public string InkingZoneDescription { get; set; }
+    }
+}
